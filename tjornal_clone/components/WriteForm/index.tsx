224fa6917f8569b80fc670383f8398d0d9857fc6
vv_tomilin <backatom1@gmail.com>
2022-04-03 20:08:25 +0500

    #1 Lesson <TJornal clone>

---
+++ tjornal_clone/components/WriteForm/index.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+import dynamic from "next/dynamic";
+
+import { Input } from '@material-ui/core';
+import styles from './WriteForm.module.scss';
+
+import { Button } from '@material-ui/core';
+
+const Editor = dynamic(() => import('../Editor'), { ssr: false });
+
+interface WriteFormProps {
+  title?: string
+}
+
+const WriteForm: React.FC<WriteFormProps> = ({ title }) => {
+
+  return (
+    <div>
+      <Input classes={{ root: styles.titleField }} placeholder="Заголовок" defaultValue={title} />
+      <div className={styles.editor}>
+        <Editor />
+      </div>
+      <Button variant="contained" color="primary">
+        Опубликвать
+      </Button>
+    </div>
+  )
+}
+
+export default WriteForm;
