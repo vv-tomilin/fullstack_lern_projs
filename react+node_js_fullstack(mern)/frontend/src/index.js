13e23982f77d2bbe5ce7ef1fa44f56844ec0c5a4
vv_tomilin <backatom1@gmail.com>
2022-09-01 07:06:08 +0500

    Добавил подготовленный шаблон проекта. Настроил роутинг, подключил Redux.toolkit <fuulstack_frontend_mern>

---
+++ react+node_js_fullstack(mern)/frontend/src/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import { Provider } from 'react-redux';
+import store from './redux/store.js';
+
+import { BrowserRouter } from 'react-router-dom';
+
+import App from './App';
+import CssBaseline from '@mui/material/CssBaseline';
+
+import './index.scss';
+import { ThemeProvider } from '@mui/material';
+import { theme } from './theme';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render(
+  <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
+      <BrowserRouter>
+        <Provider store={store}>
+          <App />
+        </Provider>
+      </BrowserRouter>
+    </ThemeProvider>
+  </React.StrictMode>
+);
