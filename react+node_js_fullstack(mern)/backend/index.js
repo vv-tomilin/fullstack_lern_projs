cd8d90d107e6c4806c188bf338cb526d76eca558
vv_tomilin <backatom1@gmail.com>
2022-09-07 20:50:54 +0500

    Запрос для тегов, отключение cors на backend <MERN>

--- react+node_js_fullstack(mern)/backend/index.js
+++ react+node_js_fullstack(mern)/backend/index.js
@@ -1,6 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import multer from 'multer';
+import cors from 'cors';
 
 import { registerValidation, loginValidation, postCreateValidation } from './validations.js';
 
@@ -29,6 +30,7 @@ const storage = multer.diskStorage({
 const upload = multer({ storage });
 
 app.use(express.json());
+app.use(cors());
 app.use('/uploads', express.static('uploads'));
 
 app.post('/auth/login', loginValidation, handleValidationErrors, UserController.login);
@@ -42,6 +44,7 @@ app.post('/uploads', checkAuth, upload.single('image'), (req, res) => {
 });
 
 app.get('/posts', PostController.getAll);
+app.get('/tags', PostController.getLastTags);
 app.get('/posts/:id', PostController.getOne);
 
 app.post('/posts', checkAuth, postCreateValidation, handleValidationErrors, PostController.create);
